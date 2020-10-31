b6a1e129c38f43f38105fc30209d990446fddc73
Faisal Anwar Nizami <faisaldev7@gmail.com>
2020-10-31 13:17:36 +0530

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ end
 def create
   @user = User.new(user_params)
   if @user.save
+    session[:user_id] = @user.id
     flash[:success] = "Welcome to the alpha blog #{@user.username}"
-    redirect_to articles_path
+    redirect_to user_path(@user)
   else
     render 'new'
   end
@@ -51,7 +52,7 @@ def require_same_user
   if !logged_in? && current_user != @user
      flash[:danger] = "You can edit your own account only"
      redirect_to root_path
-  end   
+  end
 end
 
 end
